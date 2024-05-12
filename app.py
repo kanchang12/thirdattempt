919a580b2f808e01bf1eec0fdc5d81137c5913ac
kanchang12 <32091338+kanchang12@users.noreply.github.com>
2024-05-12 09:19:04 +0100

    Update app.py

--- app.py
+++ app.py
@@ -9,9 +9,11 @@ app = Flask(__name__)
 # Define your Google Cloud project ID and access token
 #PROJECT_ID = "algebraic-ward-422922-e3"
 YOUR_ACCESS_TOKEN = os.getenv('apiKey')
+print(YOUR_ACCESS_TOKEN)
 
 @app.route('/')
 def index():
+    print('index')
     return render_template('index.html')
 
 @app.route('/submit', methods=['POST'])
@@ -19,13 +21,14 @@ def submit():
     try:
         # Extract user input from the POST request
         user_input = request.form['user_input']
+        print(user_input)
 
         # Send request to OpenAI Chat Completion API
         completion = client.chat.completions.create(
             model="gpt-3.5-turbo-16k",
             messages=[
                 {"role": "system", "content": "You are a helpful assistant."},
-                {"role": "user", "content": user_input}
+                {"role": "user", "content": "Why Sku is blue?"}
             ]
         )
 
